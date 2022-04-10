7d836493368d64c56d004fffbdfaf9fd239c8cfc
JawiliDareaAnne <dareajawili@gmail.com>
2022-04-10 23:54:36 +0800

    edit process part 2

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
+	session_start();
 	$acc_username = "Jawili_Darea";
-	$acc_passsword = "Jawili0928";
+	$acc_passsword = "jawili0928";
 	$acc_fullname = "Darea Anne Jawili";
 	$acc_address = "Marinduque PH";
 
@@ -11,6 +12,17 @@
 		if ($_REQUEST['form_username'] != $acc_username) 
 		{
 			header("Location: ".$url_add."?notexist");
+		}
+		else if ($_REQUEST['form_username'] == $acc_username && $_REQUEST['form_password'] != $acc_passsword) {
+			header("Location: ".$url_add."?wrongpass");
+		}
+		else if ($_REQUEST['form_username'] == $acc_username && $_REQUEST['form_password'] == $acc_passsword) {
+			header("Location: ".$url_add."?success");
+
+			$_SESSION['ses_username'] = $acc_username;
+			$_SESSION['ses_password'] = $acc_passsword;
+			$_SESSION['ses_fullname'] = $acc_fullname;
+			$_SESSION['ses_address'] = $acc_address;
 		}
 	}
 
@@ -51,6 +63,24 @@
 		      			if (isset($_REQUEST['notexist']) === true) 
 		      			{
 		      				echo"<div class= 'alert alert-danger' role='alert'> Username does not exist...</div>";
+		      			}
+		      			else if (isset($_REQUEST['wrongpass']) === true) 
+		      			{
+		      				echo"<div class= 'alert alert-warning' role='alert'> Incorrect password...</div>";
+		      			}
+		      			else if (isset($_REQUEST['success']) === true)
+		      			{
+		      				echo"<div class = 'alert alert-success' role='alert'> Redirecting...</div>";
+		      				header("Refresh: 5; url=account.php");
+		      			}
+		      			else if (isset($_REQUEST['logout'])===true) {
+		      				echo"<div class = 'alert alert-info' role='alert'> Thank you...</div>";
+		      			}
+		      			else if (isset($_REQUEST['logfirst'])===true) {
+		      				echo"<div class = 'alert alert-info' role='alert'>Please Login first...</div>";	      				
+		      			}
+		      			else if (isset($_SESSION['ses_username'])==true) {
+		      					echo"<div class= 'alert alert-warning' role='alert'> You are still logged in. Please <a href = 'account.php'>click here</a> to proceed.</div>";	      					
 		      			}
 		      			?>
 		      			
