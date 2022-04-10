7acbaf7fd0a5a04c59601126a002c53de9918a91
JawiliDareaAnne <dareajawili@gmail.com>
2022-04-10 12:48:24 +0800

    edit process part 1

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
+<?php
+	$acc_username = "Jawili_Darea";
+	$acc_passsword = "Jawili0928";
+	$acc_fullname = "Darea Anne Jawili";
+	$acc_address = "Marinduque PH";
+
+	$url_add = "http://".$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'];
+
+	if (isset($_REQUEST['login_button']) === true) 
+	{
+		if ($_REQUEST['form_username'] != $acc_username) 
+		{
+			header("Location: ".$url_add."?notexist");
+		}
+	}
+
+
+?>
+
 <!doctype html>
 <html lang="en">
   <head>
@@ -23,13 +42,22 @@
 		      		<span class="fa fa-user-o"></span>
 		      	</div>
 		      	<h3 class="text-center mb-4">MyTumblr Login</h3>
+
+
 						
-						<form action="#" class="login-form">
+						<form method="POST" class="login-form">
 		      		<div class="form-group">
-		      			<input type="text" class="form-control rounded-left" placeholder="Username" required>
+		      			<?php
+		      			if (isset($_REQUEST['notexist']) === true) 
+		      			{
+		      				echo"<div class= 'alert alert-danger' role='alert'> Username does not exist...</div>";
+		      			}
+		      			?>
+		      			
+		      			<input type="text" class="form-control rounded-left" placeholder="Username" name = "form_username" required>
 		      		</div>
 	            <div class="form-group d-flex">
-	              <input type="password" class="form-control rounded-left" placeholder="Password" required>
+	              <input type="password" class="form-control rounded-left" placeholder="Password" name = "form_password" required>
 	            </div>
 	            <div class="form-group d-md-flex">
 	            	<div class="w-50">
@@ -43,9 +71,11 @@
 								</div>
 	            </div>
 	            <div class="form-group">
-	            	<button type="submit" class="btn btn-primary rounded submit p-3 px-5">Get Started</button>
+	            	<button type="submit" class="btn btn-primary rounded submit p-3 px-5" name="login_button">Get Started</button>
 	            </div>
 	          </form>
+
+
 	        </div>
 				</div>
 			</div>
